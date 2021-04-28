eebd132a7c1aa421fb96d58e623125fefe48f6ec
omarelsheekh <omarelsheekh98@gmail.com>
2021-04-28 22:05:52 +0000

    gorm

--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-sqlite3 v1.14.7 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
@@ -15,4 +16,6 @@ require (
 	golang.org/x/sys v0.0.0-20210426230700-d19ff857e887 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gorm.io/driver/sqlite v1.1.4 // indirect
+	gorm.io/gorm v1.21.9 // indirect
 )
--- main.go
+++ main.go
@@ -2,13 +2,67 @@ package main
 
 import(
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+    "gorm.io/driver/sqlite"
 	"net/http"
+	"strconv"
+	_ "fmt"
 ) 
 
+type User struct {
+	ID   uint           `gorm:"primaryKey"`
+	Name string			`gorm:"unique;not null"`
+  }
+
 func main() {
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect database")
+	}
+	// Migrate the schema
+	db.AutoMigrate(&User{})
+
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.String(http.StatusOK, "hello world")
 	})
+	r.POST("/user", func(c *gin.Context) {
+		// Create
+		name := c.GetHeader("name")
+		var u User
+		if name == ""{
+			c.String(http.StatusBadRequest, "name must be included in headers")
+		} else if db.Where("name = ?", name).Find(&u).RowsAffected > 0 {
+			c.String(http.StatusBadRequest, "name %s already exists", name)
+		} else {
+			db.Create(&User{
+				Name: name,
+			})
+			c.String(http.StatusOK, "added user %s", name)
+		}
+		
+	})
+	r.GET("/user", func(c *gin.Context) {
+		var users []User
+		db.Find(&users)
+		c.JSON(http.StatusOK, gin.H{"users":users})
+	})
+	r.GET("/user/:id", func(c *gin.Context) {
+		id, err := strconv.Atoi(c.Param("id"))
+		var user User
+		if err != nil || db.First(&user, id).Error != nil{
+			// handle error
+			c.String(http.StatusNotFound, "not found")
+		}else{
+			c.JSON(http.StatusOK, gin.H{"user":user})
+		}
+	})
 	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
+
+/* References
+https://tour.golang.org/list
+https://github.com/gin-gonic/gin
+https://gorm.io/docs/index.html
+*/
