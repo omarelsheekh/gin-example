ffaa94df61d7c494fc1d57fde6a8050a6ec4ddb4
omarelsheekh <omarelsheekh98@gmail.com>
2021-04-28 13:02:42 +0000

    first commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"net/http"
+) 
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "hello world")
+	})
+	r.Run(":8080") // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+}
